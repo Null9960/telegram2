05cbe40f6120164da7d74c1dd07509e40901b412
Null9960 <93104111+Null9960@users.noreply.github.com>
2025-03-10 13:02:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def home():
 def health_check():
     return "OK", 200  # Health Check لمنع Render من فشل التشغيل
 
-if __name__ == '__main__':
-    port = int(os.getenv("PORT", 10000))  # تحديد المنفذ من البيئة
-    app.run(host="0.0.0.0", port=port)  # التأكد من أن التطبيق يستمع للمنفذ
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 1024))  # استخدام البورت المخصص من البيئة
+    app.run(host="0.0.0.0", port=port)
